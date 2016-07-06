f7648c965d7349d6812de0d15a9c226c958e3a14
Bitard Michaël <bitard.michael@gmail.com>
2016-07-06 14:55:04 +0200

    change material-ui by react-mdl

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,65 +1,10 @@
-import * as React from "react";
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import RaisedButton from 'material-ui/RaisedButton';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import ContentSortIcon from 'material-ui/svg-icons/content/sort';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
-import MenuItem from 'material-ui/MenuItem';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import Hello from "../Hello/Hello.tsx";
-import injectTapEventPlugin = require("react-tap-event-plugin");
-import {cyan500, cyan700, cyan900, lightBlue500, lightBlue700, lightBlue900} from "material-ui/styles/colors";
-
-const muiTheme = getMuiTheme(darkBaseTheme, {
-    palette: {
-            primary1Color: cyan500,
-            primary2Color: cyan700,
-            primary3Color: cyan900,
-            accent1Color: lightBlue500,
-            accent2Color: lightBlue700,
-            accent3Color: lightBlue900
-}});
-
-// palette: {
-//     primary1Color: _colors.cyan700,
-//         primary2Color: _colors.cyan700,
-//         primary3Color: _colors.grey600,
-//         accent1Color: _colors.pinkA200,
-//         accent2Color: _colors.pinkA400,
-//         accent3Color: _colors.pinkA100,
-//         textColor: _colors.fullWhite,
-//         alternateTextColor: '#303030',
-//         canvasColor: '#303030',
-//         borderColor: (0, _colorManipulator.fade)(_colors.fullWhite, 0.3),
-//         disabledColor: (0, _colorManipulator.fade)(_colors.fullWhite, 0.3),
-//         pickerHeaderColor: (0, _colorManipulator.fade)(_colors.fullWhite, 0.12),
-//         clockCircleColor: (0, _colorManipulator.fade)(_colors.fullWhite, 0.12)
-// }
+import * as React from 'react';
+import * as ReactMDL from 'react-mdl';
+import injectTapEventPlugin = require('react-tap-event-plugin');
+import Hello from '../Hello/Hello.tsx';
 
 
-// palette: {
-//     primary1Color: _colors.cyan500,
-//         primary2Color: _colors.cyan700,
-//         primary3Color: _colors.grey400,
-//         accent1Color: _colors.pinkA200,
-//         accent2Color: _colors.grey100,
-//         accent3Color: _colors.grey500,
-//         textColor: _colors.darkBlack,
-//         alternateTextColor: _colors.white,
-//         canvasColor: _colors.white,
-//         borderColor: _colors.grey300,
-//         disabledColor: (0, _colorManipulator.fade)(_colors.darkBlack, 0.3),
-//         pickerHeaderColor: _colors.cyan500,
-//         clockCircleColor: (0, _colorManipulator.fade)(_colors.darkBlack, 0.07),
-//         shadowColor: _colors.fullBlack
-// }
-
-
-
+const style = require('./App.scss');
 
 export default class App extends React.Component<{}, AppState> {
     constructor(props) {
@@ -73,50 +18,62 @@ export default class App extends React.Component<{}, AppState> {
 
     public render() {
         return (
-            <MuiThemeProvider muiTheme={muiTheme}>
-                <div>
-                    <Toolbar>
-                        <ToolbarGroup firstChild={true}>
-                            <DropDownMenu value={this.state.value} onChange={this.handleChange.bind(this)}>
-                                <MenuItem value={1} primaryText="All Broadcasts"/>
-                                <MenuItem value={2} primaryText="All Voice"/>
-                                <MenuItem value={3} primaryText="All Text"/>
-                                <MenuItem value={4} primaryText="Complete Voice"/>
-                                <MenuItem value={5} primaryText="Complete Text"/>
-                                <MenuItem value={6} primaryText="Active Voice"/>
-                                <MenuItem value={7} primaryText="Active Text"/>
-                            </DropDownMenu>
-                        </ToolbarGroup>
-                        <ToolbarGroup>
-                            <ToolbarTitle text="Options"/>
-                            <ContentSortIcon />
-                            <ToolbarSeparator />
-                            <RaisedButton label="Create Broadcast" primary={true}/>
-                            <IconMenu
-                                iconButtonElement={
-                                  <IconButton touch={true}>
-                                    <NavigationExpandMoreIcon />
-                                  </IconButton>
-                                }
-                            >
-                                <MenuItem primaryText="Download"/>
-                                <MenuItem primaryText="More Info"/>
-                            </IconMenu>
-                        </ToolbarGroup>
-                    </Toolbar>
-                    <div>
-                        {this.props.children || <Hello />}
-                    </div>
-                </div>
-            </MuiThemeProvider>
+            <ReactMDL.Layout>
+                <ReactMDL.Header className="mdl-color--grey-100 mdl-color-text--grey-600">
+                    <ReactMDL.HeaderRow title="Title">
+                        <ReactMDL.Spacer/>
+                        <ReactMDL.Textfield
+                            onChange={() => {}}
+                            label="Enter your query..."
+                            expandable
+                            expandableIcon="search"
+                        />
+                        <ReactMDL.Button raised colored id="login-menu">
+                            Toto
+                        </ReactMDL.Button>
+                        <ReactMDL.Menu target="login-menu">
+                            <ReactMDL.MenuItem>Settings</ReactMDL.MenuItem>
+                            <ReactMDL.MenuItem>Logout</ReactMDL.MenuItem>
+                        </ReactMDL.Menu>
+                    </ReactMDL.HeaderRow>
+                </ReactMDL.Header>
+                <ReactMDL.Drawer title="Menu">
+                    <ReactMDL.Navigation>
+                        <a href="">Manage</a>
+                        <a href="">Handle</a>
+                    </ReactMDL.Navigation>
+                </ReactMDL.Drawer>
+                <ReactMDL.Content className="mdl-color--grey-100">
+                    <ReactMDL.Grid>
+                        <ReactMDL.Cell shadow={1} col={8}>
+                            {this.props.children || <Hello />}
+                        </ReactMDL.Cell>
+                        <ReactMDL.Cell shadow={1} col={4} tablet={4} desktop={4}>
+                            <ReactMDL.Card style={{width: '100%'}}>
+                                <ReactMDL.CardTitle expand className={style.servercard + " mdl-color--teal-300"}>Machine</ReactMDL.CardTitle>
+                                <ReactMDL.CardText className="mdl-color-text--grey-600">
+                                    <ul>
+                                        <li>IP: <b id="machine_ip">192.168.0.1</b></li>
+                                        <li>OS: <b id="machine_os">Ubuntu 14.04</b></li>
+                                        <li>Installed applications:
+                                            <ul id="machine_installed_jobs">
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </ReactMDL.CardText>
+                                <ReactMDL.CardActions border>
+                                    <ReactMDL.Button>More</ReactMDL.Button>
+                                </ReactMDL.CardActions>
+                            </ReactMDL.Card>
+                        </ReactMDL.Cell>
+                    </ReactMDL.Grid>
+                </ReactMDL.Content>
+            </ReactMDL.Layout>
         )
     }
 
-    handleChange(event, index, value) {
-        this.setState({value: value});
-    }
 }
 
 interface AppState {
-    value?: Number;
+    value?:Number;
 }
